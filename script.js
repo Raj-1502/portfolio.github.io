a13af5fef26d2207b5a36594e1a2d2b0c3115b54
Raj-1502 <115882134+Raj-1502@users.noreply.github.com>
2023-03-24 01:51:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
 var interval;
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
 
 
 for (var i = 0; i < navMenuAnchorTags.length; i++) {
